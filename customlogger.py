8adadd2baf65ae7afa2eda0742336746f2b861c5
jyothirjt2 <79161015+jyothirjt2@users.noreply.github.com>
2022-11-09 15:26:55 +0530

    Add files via upload

---
+++ customlogger.py
@@ -0,0 +1,13 @@
+import logging
+
+
+class LogGen:
+    @staticmethod
+    def loggen():
+        logging.basicConfig(filename="C:\\Users\\DELL\\PycharmProjects\\pom-project\\Logs\\automation.log",
+                            format='%(asctime)s: %(levelname)s: %(message)s', datefmt="%d/%m/%y %I:%M:%S %p")
+        logger = logging.getLogger()
+        logger.setLevel(logging.INFO)
+        return logger
+
+
