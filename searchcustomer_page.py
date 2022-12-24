0429cc5c968bdf779b589be3e2432a1bef68aa8d
jyothirjt2 <79161015+jyothirjt2@users.noreply.github.com>
2022-12-24 16:39:15 +0530

    Add files via upload

---
+++ searchcustomer_page.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class SearchCustomer:
+    btnsearch_xpath = '//div[@class="search-text"]'
+    txtsrchemail_xpath = '//*[@id="SearchEmail"]'
+    txtfrstname_xpath = '//*[@id="SearchFirstName"]'
+    txtlstname_xpath = '//*[@id="SearchLastName"]'
+    btnsearchcustomer_xpath = '//*[@id="search-customers"]'
+
+
+
